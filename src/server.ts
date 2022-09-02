a5f027f4cc302eb895792351cbc4cf86a7ce1876
WisleyASousa <wisley.sousa@outlook.com.br>
2022-09-02 15:40:02 -0300

    Inicio do projeto

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { Request, Response, Router } from 'express';
+
+
+
+const app = express();
+const router = Router();
+
+router.get('/', (req:Request, res:Response) => {
+  res.send('World!');
+});
+
+app.use('/', router);
+app.listen
