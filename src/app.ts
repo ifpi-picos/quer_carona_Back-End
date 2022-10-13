33da611473c02af61888ca801cc0963a79bca32f
WisleyASousa <wisley.sousa@outlook.com.br>
2022-10-13 15:05:40 -0300

    realocando PrismaClient do arq app.ts para pasta database

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import routes from "./routes";
 import * as dotenv from "dotenv";
-import { PrismaClient } from "@prisma/client";
 dotenv.config();
 console.log(`The connection URL is ${process.env.DATABASE_URL}`)
-const prisma = new PrismaClient();
+
+
 const app = express();
 app.use(express.json());
 app.use('/', routes);
