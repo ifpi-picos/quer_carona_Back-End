0d84720a25117e057f82a7bd032f053288bfddab
Caio-Leto <caioletoxd@gmail.com>
2022-10-27 18:19:19 -0300

    migrations

---
+++ test/testando.test.js
@@ -0,0 +1,18 @@
+"use strict";
+describe('Testando ambiente de Teste', () => {
+    test('Teste1', () => {
+        const number = 1;
+        expect(number).toBe(1); //Nao funciona com objetos
+        expect(number).toEqual(1); //usado  para checar objetos
+    });
+    test('Teste2', () => {
+        const nome = 'Wisley';
+        expect(nome).toBe('Wisley');
+    });
+    test('Teste3 com Objeto', () => {
+        const pessoa = { nome: "Wis", idade: 22 };
+        const nPessoa = { ...pessoa };
+        expect(pessoa).toEqual(nPessoa);
+        expect(pessoa).toHaveProperty('idade'); //testando se tem tal propriedade dentro do objeto.
+    });
+});
