f4b21e8fa4b5781f9c9a8dea354a7e4da6da73af
Samuel Martins <samuel.ubuntu@gmail.com>
2023-01-19 00:45:05 -0300

    Fixing origin

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser()); 
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://quer-carona-samudebug.vercel.app/"],
   credentials: true
 }));
 
